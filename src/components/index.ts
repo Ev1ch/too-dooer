edc6545fdfd95efa9b82ae50ad15b0723144e18b
Vlad Zusko <vladzvu11@gmail.com>
2021-10-15 16:15:33 +0300

    Fixed exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,11 @@
-export default {};
+import Basic from './basic';
+import Common from './common';
+import Pages from './pages';
+import Containers from './containers';
+
+export default {
+  Basic,
+  Common,
+  Pages,
+  Containers,
+};
