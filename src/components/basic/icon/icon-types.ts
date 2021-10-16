ff5d00171959cf70114999a90ece45983627c5ff
Vlad Zusko <vladzvu11@gmail.com>
2021-10-16 13:08:43 +0300

    Added an icon

--- src/components/basic/icon/icon-types.ts
+++ src/components/basic/icon/icon-types.ts
@@ -1,9 +1,11 @@
 import tickIcon from 'assets/icons/tick.svg';
 import plusIcon from 'assets/icons/plus.svg';
+import crossIcon from 'assets/icons/cross.svg';
 
 const EIconTypes = {
   TICK: tickIcon,
   PLUS: plusIcon,
+  CROSS: crossIcon,
 };
 
 export default EIconTypes;
