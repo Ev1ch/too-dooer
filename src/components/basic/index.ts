4b4922eade03c9d429171d7aaf73f0d03f07e3b8
Vlad Zusko <vladzvu11@gmail.com>
2021-10-18 14:56:05 +0300

    Added modal component

--- src/components/basic/index.ts
+++ src/components/basic/index.ts
@@ -1,3 +1,4 @@
 export { default as Icon } from './icon';
 export { default as Checkbox } from './checkbox';
 export { default as AddButton } from './add-button';
+export { default as Modal } from './modal';
---
+++ src/components/basic/modal/index.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useEffect, useState } from 'react';
+import { Icon } from 'components';
+import Icons from 'components/basic/icon/icon-types';
+import styles from './modal.module.scss';
+import clsx from 'clsx';
+
+interface IModalProps {
+  children?: ReactNode;
+  state?: boolean;
+  title?: string;
+  onClose?: () => void;
+}
+
+function Modal({ children, state, title, onClose }: IModalProps) {
+  const [isActive, setIsActive] = useState(state === undefined ? true : state);
+
+  const onCloseHandler = () => {
+    setIsActive(false);
+
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    setIsActive(state || false);
+  }, [state]);
+
+  return (
+    <div className={clsx(styles.modalContainer, isActive && styles.modalActive)}>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <span className={styles.modalTitle}>{title}</span>
+          <button className={styles.modalClose} onClick={onCloseHandler}>
+            <Icon icon={Icons.CROSS} />
+          </button>
+        </div>
+        <div className={styles.modalContent}>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
