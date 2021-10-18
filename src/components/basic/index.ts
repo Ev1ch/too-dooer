cc5b482ada675d9d763aa4c6245c43234952d6c8
Vlad Zusko <vladzvu11@gmail.com>
2021-10-18 22:05:50 +0300

    Added basic divider

--- src/components/basic/index.ts
+++ src/components/basic/index.ts
@@ -5,3 +5,4 @@ export { default as Modal } from './modal';
 export { default as Textarea } from './textarea';
 export { default as Input } from './input';
 export { default as Button } from './button';
+export { default as Divider } from './divider';
