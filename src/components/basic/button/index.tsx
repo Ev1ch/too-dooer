09d41373b970a0e2194d074ab9400e58fe4bcd6b
Vlad Zusko <vladzvu11@gmail.com>
2021-10-18 21:35:24 +0300

    Added button disabled styles

--- src/components/basic/button/index.tsx
+++ src/components/basic/button/index.tsx
@@ -4,11 +4,12 @@ import styles from './button.module.scss';
 interface IButtonProps {
   children?: ReactNode;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  disabled: boolean;
 }
 
-function Button({ children, onClick }: IButtonProps) {
+function Button({ children, onClick, disabled }: IButtonProps) {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
