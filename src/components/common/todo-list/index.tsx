cd4c64e198a8a8a87595e2070000a2c7956b2eb9
Vlad Zusko <vladzvu11@gmail.com>
2021-10-18 21:37:26 +0300

    Added todo list component

---
+++ src/components/common/todo-list/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ITodoItem } from 'types';
+import TodoItem from './todo-item';
+import styles from './todo-list.module.scss';
+
+interface ITodoListProps {
+  items: ITodoItem[];
+  onTodoStateChange?: (id: string) => void;
+  onTodoDelete?: (id: string) => void;
+  onTodoEdit?: (id: string, updatedTodo: Partial<ITodoItem>) => void;
+}
+
+function TodoList({ items, onTodoStateChange, onTodoEdit, onTodoDelete }: ITodoListProps): JSX.Element {
+  return (
+    <div className={styles.todoList}>
+      {items.map((item) => (
+        <TodoItem
+          item={item}
+          onTodoStateChange={onTodoStateChange}
+          onTodoEdit={onTodoEdit}
+          onTodoDelete={onTodoDelete}
+          key={item.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
