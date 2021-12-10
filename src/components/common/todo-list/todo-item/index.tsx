330a285585492bffbab61cd88987660a90a91dbe
Vlad Zusko <vladzvu11@gmail.com>
2021-12-10 16:24:32 +0200

    Removed useless effect

--- src/components/common/todo-list/todo-item/index.tsx
+++ src/components/common/todo-list/todo-item/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { Checkbox, Icon, Textarea, Input } from 'components';
 import Icons from 'components/basic/icon/icon-types';
